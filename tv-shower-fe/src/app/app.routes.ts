9668602a065e329d440e6f8b9fe7830b658b8cca
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-28 23:20:20 +0200

    feat:header

--- tv-shower-fe/src/app/app.routes.ts
+++ tv-shower-fe/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'list',
+    component:ListComponent,
+  },   { path: '',   component:AppComponent }, 
+]; 
